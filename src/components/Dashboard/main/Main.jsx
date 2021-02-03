f18ace36404c27ca2d5527a3f2bdcb3ab3d09c10
maiconmiranda <cam022021@coderacademy.edu.au>
2021-02-04 09:40:10 +1100

    Create Company Dashboard

---
+++ src/components/Dashboard/main/Main.jsx
@@ -0,0 +1,117 @@
+import "./Main.css";
+import car_on_road from "../../assets/car_on_road.svg";
+import Chart from "../charts/Chart";
+
+const Main = () => {
+  return (
+    <main>
+      <div className="main__container">
+        {/* <!-- MAIN TITLE STARTS HERE --> */}
+
+        <div className="main__title">
+          <img src={car_on_road} alt="car_on_road" />
+          <div className="main__greeting">
+            <h1>Hello "User Name"</h1>
+            <p>Welcome to your admin dashboard</p>
+          </div>
+        </div>
+
+        {/* <!-- MAIN TITLE ENDS HERE --> */}
+
+        {/* <!-- MAIN CARDS STARTS HERE --> */}
+        <div className="main__cards">
+          <div className="card">
+            <i
+              className="fa fa-user-o fa-2x text-lightblue"
+              aria-hidden="true"
+            ></i>
+            <div className="card_inner">
+              <p className="text-primary-p">Drivers</p>
+              <span className="font-bold text-title">34</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <i className="fa fa-calendar fa-2x text-red" aria-hidden="true"></i>
+            <div className="card_inner">
+              <p className="text-primary-p">Daily Tracks</p>
+              <span className="font-bold text-title">130</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <i
+              className="fa fa-car fa-2x text-blue"
+              aria-hidden="true"
+            ></i>
+            <div className="card_inner">
+              <p className="text-primary-p">Condition Reports</p>
+              <span className="font-bold text-title">25</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <i
+              className="fa fa-car fa-2x text-red"
+              aria-hidden="true"
+            ></i>
+            <div className="card_inner">
+              <p className="text-primary-p">Incidents</p>
+              <span className="font-bold text-title">2</span>
+            </div>
+          </div>
+        </div>
+        {/* <!-- MAIN CARDS ENDS HERE --> */}
+
+        {/* <!-- CHARTS STARTS HERE --> */}
+        <div className="charts">
+          <div className="charts__left">
+            <div className="charts__left__title">
+              <div>
+                <h1>Daily Reports</h1>
+                <p>Total expenses</p>
+              </div>
+              <i className="fa fa-usd" aria-hidden="true"></i>
+            </div>
+            <Chart />
+          </div>
+
+          <div className="charts__right">
+            <div className="charts__right__title">
+              <div>
+                <h1>Stats Reports</h1>
+                <p>"Company name"</p>
+              </div>
+              <i className="fa fa-usd" aria-hidden="true"></i>
+            </div>
+
+            <div className="charts__right__cards">
+              <div className="card1">
+                <h1>Fuel costs</h1>
+                <p>$1,300.00</p>
+              </div>
+
+              <div className="card2">
+                <h1>Parking fees</h1>
+                <p>$120.00</p>
+              </div>
+
+              <div className="card3">
+                <h1>Fines</h1>
+                <p>1,900.00</p>
+              </div>
+
+              <div className="card4">
+                <h1>Oders</h1>
+                <p>1,88.10</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <!-- CHARTS ENDS HERE --> */}
+      </div>
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/Dashboard/sidebar/Sidebar.jsx
@@ -0,0 +1,101 @@
+import "./Sidebar.css"
+import on_scooter from "../../assets/on_scooter.svg"
+
+
+const Sidebar = ({ sidebarOpen, closeSidebar }) => {
+  return (
+    <div className={sidebarOpen ? "sidebar_responsive" : ""} id="sidebar">
+      <div className="sidebar__title">
+        <div className="sidebar__img">
+          <img src={on_scooter} alt="logo" />
+          <h1>fleets</h1>
+          <hr/>
+          <h3>Company Area</h3>
+        </div>
+        <i
+          onClick={() => closeSidebar()}
+          className="fa fa-times"
+          id="sidebarIcon"
+          aria-hidden="true"
+        ></i>
+      </div>
+
+      <div className="sidebar__menu">
+        <div className="sidebar__link active_menu_link">
+          <i className="fa fa-home"></i>
+          <a href="/">Dashboard</a>
+        </div>
+        <h2>ACCOUNT</h2>
+        <div className="sidebar__link">
+          <i className="fa fa-address-book" aria-hidden="true"></i>
+          <a href="/">Update Company Details</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-cog"></i>
+          <a href="/">Settings</a>
+        </div>
+        {/* <div className="sidebar__link">
+          <i className="fa fa-wrench"></i>
+          <a href="/">Employee Management</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-archive"></i>
+          <a href="/">Warehouse</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-handshake-o"></i>
+          <a href="/">Contracts</a>
+        </div> */}
+        <h2>DRIVERS</h2>
+        <div className="sidebar__link">
+          <i className="fa fa-id-card"></i>
+          <a href="/">View All Drivers</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-plus"></i>
+          <a href="/">Add a Driver</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-edit"></i>
+          <a href="/">Edit a Driver</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-minus"></i>
+          <a href="/">Remove a driver</a>
+        </div>
+        <h2>VEHICLE</h2>
+        <div className="sidebar__link">
+          <i className="fa fa-car"></i>
+          <a href="/">View Fleet</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-plus"></i>
+          <a href="/">Add a Vehicle</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-edit"></i>
+          <a href="/">Edit Vehicle</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-minus"></i>
+          <a href="/">Remove Vehicle</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-link"></i>
+          <a href="/">Assign Driver to Vehicle</a>
+        </div>
+        <h2>REPORTS</h2>
+        <div className="sidebar__link">
+          <i className="fa fa-database"></i>
+          <a href="/">View Reports</a>
+        </div>
+        <div className="sidebar__logout">
+          <i className="fa fa-power-off"></i>
+          <a href="/">Log out</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
