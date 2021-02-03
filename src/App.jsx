1092af5de57b08127327f423d6678f842d1b2a01
maiconmiranda <cam022021@coderacademy.edu.au>
2021-02-04 09:29:49 +1100

    Add routesto App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Dashboard from "./components/dashboardLink/Dashboard";
+import { Home } from "./components/home/Home";
+import { HowItWorks } from "./components/howItWorks/HowItWorks";
+import { LogIn } from "./components/logIn/LogIn";
+import { CreateAccount } from "./components/signUp/CreateAccount";
+import { SignUp } from "./components/signUp/SignUp";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/company" component={Dashboard} />
+      <Route path="/how-it-works" component={HowItWorks} />
+      <Route path="/login" component={LogIn} />
+      <Route path="/create-account" component={CreateAccount} />
+      <Route path="/sign-up" component={SignUp} />
+    </Switch>
   );
-}
+};
 
 export default App;
